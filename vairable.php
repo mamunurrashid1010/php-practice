93951ca815b84aef6a804892666e56ab3960aad9
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-05-28 15:33:37 +0600

    variable declaration, assign value, print,concatenation

---
+++ vairable.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+<body>
+
+    <h3>PHP variable Example</h3><br>
+
+    <?php
+
+        # variable declaration with value
+        $txt = "Hello world!";
+        $x = 10;
+        $y = 5.5;
+
+        # print
+        echo $txt;
+        echo "<br>";
+        echo $x;
+        echo "<br>";
+        echo $y;
+
+        #concatenation
+        echo "<br>"."php concatenation example"."<br>";
+        echo "Text:".$txt."Value X:".$x."Value Y:".$y;
+    ?>
+
+</body>
+</html>
