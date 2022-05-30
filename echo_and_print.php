186de41cd47cba5211bc93cd7924916575e524d0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-05-30 10:26:27 +0600

    print output example

--- echo_and_print.php
+++ echo_and_print.php
@@ -9,10 +9,19 @@
 
     <?php
         # we can use echo or print to get output.
+        # echo output
         echo "<h3>This is the example of echo output</h3>";
         echo "Hello World!<br>";
         echo "I'm about to learn PHP!<br>";
         echo "-----------------------------------";
+
+        # print output
+        print "<h3>This is the example of print output</h3>";
+        print "Hello World!<br>";
+        print "I'm about to learn PHP!<br>";
+        print "-----------------------------------";
+
+        
 
     ?>
 
