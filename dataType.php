e15ce4d5a8e0d6346f5f7e3440401e481972264c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-05-31 09:59:33 +0600

    php different data type example

---
+++ dataType.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Data Type Example</h3><br>
+
+    <?php
+
+        /**
+         * This is example of php data type.
+         * PHP supports the following data types:
+            String
+            Integer
+            Float (floating point numbers - also called double)
+            Boolean
+            Array
+            Object
+            NULL
+            Resource
+         */
+        echo "<h4>String Data type:</h4>";
+        $string="This is string data type.";
+        echo $string;
+
+
+    ?>
+
+</body>
+</html>
