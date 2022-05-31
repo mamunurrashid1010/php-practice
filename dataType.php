c7c754ff9efdc06afe68007119bd9e76eceeaeea
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-05-31 10:03:18 +0600

    update

--- dataType.php
+++ dataType.php
@@ -8,7 +8,6 @@
 <h3>PHP Data Type Example</h3><br>
 
     <?php
-
         /**
          * This is example of php data type.
          * PHP supports the following data types:
@@ -25,6 +24,19 @@
         $string="This is string data type.";
         echo $string;
 
+        echo "<h4>Integer Data type:</h4>";
+        $integer=120;
+        var_dump($integer) ;
+
+        echo "<h4>Float Data type:</h4>";
+        $float=120.55;
+        var_dump($float) ;
+
+        echo "<h4>Boolean Data type:</h4>";
+        $x=true;
+        $y=false;
+        var_dump($x) ;
+        var_dump($y) ;
 
     ?>
 
