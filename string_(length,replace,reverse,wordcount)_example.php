a5b14474ae3f25a55516fc98ce23e8c1e958594e
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-01 10:11:02 +0600

    string word count example add

--- string_(length,replace,reverse,wordcount)_example.php
+++ string_(length,replace,reverse,wordcount)_example.php
@@ -12,11 +12,18 @@
          * This is example of php String different funtion to manipulate string.
          * string manipulate function:
             1. strlen()
+            2. str_word_count()
          */
 
+        # string length
         echo "<h4>String Length:</h4>";
         $string="Hello World";
         echo $string."<br>Length: ".strlen($string);
+
+        # string word count
+        echo "<h4>String word count:</h4>";
+        $string="Hello World";
+        echo $string."<br>Length: ".str_word_count($string);
 
     ?>
 
