f58761f8df8dedf61bf23cb516e4d392915883fe
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-01 10:08:24 +0600

    string length calculation

---
+++ string_(length,replace,reverse,wordcount)_example.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP String Manipulate function</h3><br>
+
+    <?php
+        /**
+         * This is example of php String different funtion to manipulate string.
+         * string manipulate function:
+            1. strlen()
+         */
+
+        echo "<h4>String Length:</h4>";
+        $string="Hello World";
+        echo $string."<br>Length: ".strlen($string);
+
+    ?>
+
+</body>
+</html>
