e13e082af29d21611de3279ce9ccfaf3ae40bda3
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-02 10:16:57 +0600

    number and default datatype check function example

---
+++ number_dataTyceCheck.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Number and Check Datatype</h3><br>
+
+    <?php
+        /**
+         * This is example of php number and different function of dataType check.
+         * Data type check function:
+            1. is_int()
+         */
+
+        # integer check
+        echo "<h4>Check if the type of a variable is integer:</h4>";
+        $number=123;
+        var_dump(is_int($number));
+
+
+    ?>
+
+</body>
+</html>
