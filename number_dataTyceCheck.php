9905b4c03cbe7422d549bb9ca96772fc1947b349
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-02 14:54:58 +0600

    number dataType check

--- number_dataTyceCheck.php
+++ number_dataTyceCheck.php
@@ -12,13 +12,26 @@
          * This is example of php number and different function of dataType check.
          * Data type check function:
             1. is_int()
+            2. is_float()
          */
 
         # integer check
         echo "<h4>Check if the type of a variable is integer:</h4>";
         $number=123;
+        echo $number."<br>";
         var_dump(is_int($number));
 
+        # float check
+        echo "<h4>Check if the type of a variable is float:</h4>";
+        $number=123.55;
+        echo $number."<br>";
+        var_dump(is_float($number));
+
+        # finite or infinite check
+        echo "<h4>Check if the type of a numeric value is finite or infinite:</h4>";
+        $number=1.9e411;
+        echo $number."<br>";
+        var_dump($number);
 
     ?>
 
