4d3d757f0953e25d986a7cd5a8a01734ad659c0c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-04 15:32:47 +0600

    math function of php

---
+++ math_function.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Math Function</h3><br>
+
+    <?php
+        /**
+         * This is example of php different math function.
+            1. sqrt()
+         */
+
+        # sqrt function
+        echo "<h4>sqrt function:</h4>";
+        $number=25;
+        echo $number."<br>square root: ".sqrt($number);
+
+    ?>
+
+</body>
+</html>
