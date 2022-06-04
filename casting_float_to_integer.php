91bee43429e042eb5ef39cec5a3971023702407e
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-04 09:49:17 +0600

    convert float to integer value

---
+++ casting_float_to_integer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP String Casting</h3><br>
+
+    <?php
+        /**
+         * This is example of php casting string and how to convert float to integer.
+         */
+
+        # convert float to integer
+        echo "<h4>convert float to integer:</h4>";
+        $number=10.556;
+        echo $number."<br>convert to integer: ".(int)$number;
+
+    ?>
+
+</body>
+</html>
