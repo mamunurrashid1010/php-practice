7567b8cdd7200c23b9f5d8157101e225f3708604
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-04 15:37:15 +0600

    add round function example

--- math_function.php
+++ math_function.php
@@ -11,12 +11,21 @@
         /**
          * This is example of php different math function.
             1. sqrt()
+            2. round()
          */
 
         # sqrt function
         echo "<h4>sqrt function:</h4>";
         $number=25;
         echo $number."<br>square root: ".sqrt($number);
+
+        # round function
+        echo "<h4>Round function:</h4>";
+        echo "<h5>The round() function rounds a floating-point number to its nearest integer:</h5>";
+        $number1=0.7;
+        $number2=0.4;
+        echo $number1."<br>round is: ".round($number1)."<br>";
+        echo $number2."<br>round is: ".round($number2);
 
     ?>
 
