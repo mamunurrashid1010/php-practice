2f0b1b0bc8788cef03f7cca613efcd457e20e779
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-06 17:46:31 +0600

    switch condition example

---
+++ switch_condition.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Switch Condition</h3><br>
+
+    <?php
+        /**
+         * This is example of php switch condition.
+         */
+
+        # switch condition example
+        echo "<h4>Switch condition example:</h4>";
+        $color="white";
+        switch ($color){
+            case "red":
+                echo "The value is: " .$color;
+                break;
+            case "green":
+                echo "The value is: " .$color;
+                break;
+            case "blue":
+                echo "The value is: " .$color;
+                break;
+            case "white":
+                echo "The value is: " .$color;
+                break;
+            case "black":
+                echo "The value is: " .$color;
+                break;
+        }
+
+
+    ?>
+
+</body>
+</html>
