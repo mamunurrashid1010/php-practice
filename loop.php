ada74eb3c9890b653222e5a919e862cbc0409171
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-07 10:01:43 +0600

    while loop example

---
+++ loop.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Loop Example</h3><br>
+
+    <?php
+        /**
+         * This is example of php Loop.
+            1. while loop
+         */
+
+        # syntax
+        echo "<h4>While loop:</h4>";
+        $x=1;
+        while ($x<=5){
+            echo $x;
+            $x++;
+        }
+
+    ?>
+
+</body>
+</html>
