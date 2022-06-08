0a85d3547dba8d652edf9d1b3d5eac06c2827165
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-08 11:27:03 +0600

    array example

---
+++ array.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Array</h3><br>
+
+    <?php
+        /**
+         * This is an example of php array.
+         * There are 3 types of array
+            1. Indexed Array
+            2. Associative Array
+            3. Multidimensional  Array
+         */
+
+        # array example
+        echo "<h4>array example:</h4>";
+        $studentName=array('hasan','emon','rony','kamal','jamal');
+        echo $studentName[0]."<br>";
+        echo $studentName[1]."<br>";
+        echo $studentName[3]."<br>";
+
+
+    ?>
+
+</body>
+</html>
