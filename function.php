5e825cb99eb665df1d82c92b4b6da7ba5caff9c0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-08 11:03:35 +0600

    function example

---
+++ function.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Function</h3><br>
+
+    <?php
+        /**
+         * This is an example of php user define function.
+         */
+
+        # function example
+        echo "<h4>user define function example:</h4>";
+        function hello(){
+            echo "I am from hello function";
+        }
+
+        hello();
+
+    ?>
+
+</body>
+</html>
