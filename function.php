64ae6520ead3e02c3b0a2887fae7a3965256cf01
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-08 11:08:31 +0600

    function arguments example

--- function.php
+++ function.php
@@ -18,7 +18,15 @@
             echo "I am from hello function";
         }
 
+        # function example
+        function sum($value1,$value2){
+            echo "<h4>php function arguments example:</h4>";
+            echo "The sum of ".$value1 ." and ".$value2 ." is: ".($value1+$value2) ;
+        }
+
+        # function calling
         hello();
+        sum(10,20)
 
     ?>
 
