f273f1261d6302f158837cd9cb51aa393bbbb643
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-11 10:06:39 +0600

    super global variable

---
+++ global_variable.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Global Variable</h3><br>
+
+    <?php
+        /**
+         * This is example of php global variable.
+         * global variable are:
+            $GLOBALS
+            $_SERVER
+            $_REQUEST
+            $_POST
+            $_GET
+            $_FILES
+            $_ENV
+            $_COOKIE
+            $_SESSION
+         */
+
+      
+
+
+    ?>
+
+</body>
+</html>
