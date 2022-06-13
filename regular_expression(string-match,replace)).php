6d238c238989c2c608087718c7627058158d62c8
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-13 10:04:52 +0600

    string match in a text, example added

---
+++ regular_expression(string-match,replace)).php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Practice</title>
+</head>
+<body>
+
+<h3>PHP Regular Expression</h3><br>
+
+    <?php
+        /**
+         * This is example of php regular expression.
+         * PHP provides a variety of functions that allow you to use regular expressions.
+            1.preg_match()
+            2.preg_match_all()
+            3.preg_replace()
+         */
+
+        # preg_match() function example
+        echo "<h4>Regular Expression preg_match() example:</h4>";
+        $text="hello world";
+        $pattern="/world/i";
+        $result=preg_match($pattern,$text);
+        if($result==1)
+            echo "found";
+        else
+            echo "not found";
+
+
+
+
+    ?>
+
+</body>
+</html>
