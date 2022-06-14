4e1711521ba4a62153d4b0442333640720c5a3b8
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-14 10:33:30 +0600

    php date function example

---
+++ advanced/date_time.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+     * This is an example of php data and time.
+     */
+
+    # date example
+    echo "Today date is: ".date('d-m-y').'<br>';
+    echo "Today date is: ".date('d/m/y').'<br>';
+
