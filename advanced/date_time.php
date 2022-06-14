2ece2bd11452698b42f0c17c3993102a7753d6b0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-14 10:37:51 +0600

    This moth, day, year example

--- advanced/date_time.php
+++ advanced/date_time.php
@@ -4,6 +4,13 @@
      */
 
     # date example
+    echo "Date Example:<br>";
     echo "Today date is: ".date('d-m-y').'<br>';
     echo "Today date is: ".date('d/m/y').'<br>';
+    echo "Today date is: ".date('d.m.y').'<br><br>';
 
+    # this month day/month/year.
+    echo "Day/Month/Year Example:<br>";
+    echo "This month day: ".date('d').'<br>';
+    echo "This month: ".date('m').'<br>';
+    echo "This year: ".date('Y').'<br>';
