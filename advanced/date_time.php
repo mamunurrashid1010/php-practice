c02719a852f632a690d09c3ed3e29bf1e3a55d88
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-14 10:46:25 +0600

    time and time zone set example

--- advanced/date_time.php
+++ advanced/date_time.php
@@ -13,4 +13,13 @@
     echo "Day/Month/Year Example:<br>";
     echo "This month day: ".date('d').'<br>';
     echo "This month: ".date('m').'<br>';
-    echo "This year: ".date('Y').'<br>';
+    echo "This year: ".date('Y').'<br><br>';
+
+    # time example
+    echo "Time Example:<br>";
+    echo "Time: ".date('h:i:sa').'<br><br>';
+
+    # time zone set example
+    echo "Timezone set (Asia/Dhaka) Example:<br>";
+    echo date_default_timezone_set('Asia/Dhaka');
+    echo "Time: ".date('h:i:sa').'<br>';
