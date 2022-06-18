16e1b6beed6dd0810fa10c0540ee5d7998556b4b
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-18 12:03:42 +0600

    write a file example

--- advanced/file.php
+++ advanced/file.php
@@ -8,3 +8,15 @@
     $file=fopen("testFile.txt","r") or die('Unable to open');
     echo fgets($file);
     fclose($file);
+
+    # write a file example
+    echo "<br>Write a file example:<br>";
+    $file=fopen("testFile.txt","w") or die("Unable to open file");
+    $text="Mamunur rashid\n";
+    fwrite($file,$text);
+    $text="B.Sc in CSE\n";
+    fwrite($file,$text);
+
+    echo fgets($file);
+    fclose($file);
+
