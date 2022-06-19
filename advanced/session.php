a141952b210af19c05e0ba1433c441913e3adac9
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-19 10:53:46 +0600

    session value print, overwrite, remove session value, destroy session value

--- advanced/session.php
+++ advanced/session.php
@@ -10,3 +10,19 @@
     $_SESSION['name']="mamun";
     $_SESSION['mobile']="01812555555";
 
+    # print session value
+    echo "Name :".$_SESSION['name']."<br>";
+    echo "Mobile :".$_SESSION['mobile']."<br>";
+    echo "<pre>";
+    print_r($_SESSION);
+
+    # overwrite session value
+    $_SESSION['mobile']="0181000000";
+    echo "Mobile :".$_SESSION['mobile']."<br>";
+
+    # remove session all value
+    session_unset();
+    print_r($_SESSION);
+
+    # destroy session
+    session_destroy();
