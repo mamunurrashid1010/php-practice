1b12c5d1c1e0b82f86f12e843dc297156bafdb93
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-19 10:45:53 +0600

    session example

---
+++ advanced/session.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+     * This is an example of php file session.
+     */
+
+    # session start example
+    session_start();
+
+    # set session value
+    $_SESSION['name']="mamun";
+    $_SESSION['mobile']="01812555555";
+
