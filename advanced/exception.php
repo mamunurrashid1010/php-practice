ccfda4d8b48a3120ed20ee600f215017e9bc77d7
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-22 10:08:24 +0600

    try catch block example added

--- advanced/exception.php
+++ advanced/exception.php
@@ -3,12 +3,18 @@
      * This is an example of php exception
      */
 
-    # exception example
+    # exception example-1
     function divide($dividend, $divisor) {
         if($divisor == 0) {
             throw new Exception("Division by zero");
         }
         return $dividend / $divisor;
     }
+    //echo divide(5, 0);
 
-    echo divide(5, 0);
+    # exception example-2
+    try{
+        echo divide(5, 0);
+    }catch (Exception $e){
+        echo "Unable to divide";
+    }
