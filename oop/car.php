ee4160b62e730ede4b54dcb23754df5a4b9a80a9
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-23 10:08:36 +0600

    php class example added

---
+++ oop/car.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This is example of php class.
+ */
+class car{
+    # Properties
+    public $name;
+    public $color;
+
+    # Method
+    function setName($name){
+        $this->name=$name;
+    }
+
+}
