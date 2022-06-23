abe15048e3bbc23bfd90e1ee43b0cda2f07de340
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-23 10:16:07 +0600

    class object practice example added

--- oop/car.php
+++ oop/car.php
@@ -8,8 +8,19 @@ class car{
     public $color;
 
     # Method
-    function setName($name){
+    public function setName($name){
         $this->name=$name;
     }
 
 }
+
+    # Object create
+    $carObj=new car();
+
+    # set value
+    $carObj->setName('Toyota');
+    $carObj->color="White";
+
+    # get value
+    echo $carObj->name."<br>";
+    echo $carObj->color;
