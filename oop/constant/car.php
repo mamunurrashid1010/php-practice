8864bd748a07fa0bce7d7219285192b4ec9b9b9d
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-28 10:21:48 +0600

    update constant example

--- oop/constant/car.php
+++ oop/constant/car.php
@@ -4,7 +4,12 @@
  */
 
 class car{
-    const modelNumber=" AB12555";
+    const modelNumber="AB12555";
+    public static function carInfo(){
+        echo "<br>Car Info:<br>";
+        echo "BMW, AB12555, dark-blue";
+    }
 }
 
 echo car::modelNumber;
+echo car::carInfo();
