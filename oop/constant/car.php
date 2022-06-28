e8575832dc9ff1cc2429ce02cdd63b52813bb9b5
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-06-28 10:28:45 +0600

    constant. scopresolution and object example added

--- oop/constant/car.php
+++ oop/constant/car.php
@@ -9,7 +9,21 @@ class car{
         echo "<br>Car Info:<br>";
         echo "BMW, AB12555, dark-blue";
     }
+
+    public static function model(){
+        echo "<br>Model:<br>";
+        echo self::modelNumber;
+    }
 }
 
+# method-1
+echo "<br>------------------ Method 1 ------------------<br>";
 echo car::modelNumber;
-echo car::carInfo();
+ car::carInfo();
+ car::model();
+
+ echo "<br>------------------ Method 2 ------------------<br>";
+# method-2, create object
+$obj=new car();
+$obj->carInfo();
+$obj->model();
