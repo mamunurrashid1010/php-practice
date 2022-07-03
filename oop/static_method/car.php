9d23ecd6b097b48c79116b53efc78c3e4d2ad888
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-03 10:20:32 +0600

    static method example-2

--- oop/static_method/car.php
+++ oop/static_method/car.php
@@ -5,14 +5,34 @@
     Static methods can be called directly - without creating an instance of the class first.
     Static methods are declared with the static keyword
  */
+echo "<h4>PHP- Static Method Example:</h4>";
 
+
+
+echo "-------------------------- Example-1 --------------------------------------";
 # class
 class car{
     public static function carInfo(){
-        echo "I'm from static method class";
+        echo "<br>I'm from static method class<br>";
     }
-
 }
-
 # call static method
 car::carInfo();
+
+
+
+
+echo "-------------------------- Example-2 --------------------------------------";
+# class
+class owner{
+    public static function ownerName(){
+        echo "<br>Owner: Mamunur Rashid<br>";
+    }
+
+    public function __construct(){
+        $this->ownerName();
+    }
+}
+
+# class instance
+new owner();
