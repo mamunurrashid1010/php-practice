b9db7fb7b2d4ff8f082525b14082bbe5d585bf05
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-04 10:22:22 +0600

    static properties use different method, example 3

--- oop/static_properties/car.php
+++ oop/static_properties/car.php
@@ -25,6 +25,7 @@ echo "<h5>-------------------------- Example-2 ---------------------------------
 # class
 class pi{
     public static $value="3.1416";
+    public  $value1="3.1416";
     public function staticValue(){
         echo self::$value;
     }
@@ -33,3 +34,28 @@ class pi{
 # class object create
 $piObj=new pi();
 $piObj->staticValue();
+
+
+echo "<h5>-------------------------- Example-3 ------------------------------------</h5>";
+
+# parent class inheritance
+class practice extends pi{
+    public function printPIValue(){
+        //properties access
+        echo parent::$value ."<br>";
+        echo $this->value1 ."<br><br>";
+        //method access
+        $this->staticValue();
+        echo "<br>";
+        parent::staticValue();
+    }
+}
+# get value method-1
+$practiceObj=new practice();
+$practiceObj->printPIValue();
+
+echo "<br>". $practiceObj->value1 ."<br>";
+
+# get static value method-2
+echo "<br><br>";
+echo practice::$value;
