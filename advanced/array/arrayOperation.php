9d73fe3ced77198d01d51a2548c8d06ca88002dd
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-05 11:19:51 +0600

    array operation, array print and array value add using array_push() function

--- advanced/array/arrayOperation.php
+++ advanced/array/arrayOperation.php
@@ -6,9 +6,24 @@
 class arrayOperation{
     public $arr=[10,20,30,40,50];
 
+    function printArray(){
+        echo "Array:<br>";
+        for($i=0;$i<count($this->arr);$i++){
+            echo $this->arr[$i].", ";
+        }
+        echo "<br><br>";
+    }
+
     function valueCount(){
         echo "Array Length:<br>";
         echo count($this->arr);
+        echo "<br><br>";
+    }
+
+    function valueAdd($value){
+        echo "Array Value Add:<br>";
+        echo $value."<br>";
+        array_push($this->arr,$value);
         echo "<br><br>";
     }
 }
@@ -16,5 +31,14 @@ class arrayOperation{
 # class instance create
 $obj=new arrayOperation();
 
+echo "<h4>Array Operation</h4>";
+
+# array print
+$obj->printArray();
+
 # array count value
 $obj->valueCount();
+
+# array value add
+$obj->valueAdd(60);
+$obj->printArray();
