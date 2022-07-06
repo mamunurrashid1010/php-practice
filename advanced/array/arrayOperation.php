7a64bcbe3e9865dd52354d8e917678bca54f622c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-06 09:51:30 +0600

    delete last element from array using array_pop() function

--- advanced/array/arrayOperation.php
+++ advanced/array/arrayOperation.php
@@ -6,6 +6,7 @@
 class arrayOperation{
     public $arr=[10,20,30,40,50];
 
+    # array print
     function printArray(){
         echo "Array:<br>";
         for($i=0;$i<count($this->arr);$i++){
@@ -14,17 +15,28 @@ class arrayOperation{
         echo "<br><br>";
     }
 
+    # array length
     function valueCount(){
         echo "Array Length:<br>";
         echo count($this->arr);
         echo "<br><br>";
     }
 
+    # push/add data to array
     function valueAdd($value){
         echo "Array Value Add:<br>";
         echo $value."<br>";
         array_push($this->arr,$value);
         echo "<br><br>";
+    }
+
+    # pop/delete last element of the array
+    function lastElementDelete(){
+        echo "Array Last Element Delete:<br>";
+        $length= count($this->arr);
+        echo "Last Element: ".$this->arr[$length-1]."<br>";
+        array_pop($this->arr);
+        echo "<br>";
     }
 }
 
@@ -39,6 +51,10 @@ $obj->printArray();
 # array count value
 $obj->valueCount();
 
-# array value add
+# array add new value
 $obj->valueAdd(60);
+$obj->printArray();
+
+# pop/delete last element of the array
+$obj->lastElementDelete();
 $obj->printArray();
