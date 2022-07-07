2471961b9159de4ca6eec92f9054bcc6dbf75e7a
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-07 10:12:48 +0600

    day of week example

--- advanced/calendar/calendar.php
+++ advanced/calendar/calendar.php
@@ -10,4 +10,9 @@
     # Get the number of days in a month for a specified year and calendar
     echo "Number of day in a month:<br>";
     $day=cal_days_in_month(CAL_GREGORIAN,2,2010);
-    echo "There was $day days in  February 2010<br>";
+    echo "There was $day days in  February 2010<br><br>";
+
+    # The jddayofweek() function returns the day of the week
+    echo "day of the week (7,7,2022):<br>";
+    $jd=gregoriantojd(7,7,2022);
+    echo jddayofweek($jd,1)."<br><br>";
