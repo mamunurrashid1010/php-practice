fd59b91cec421f6de8a3c3ba928f116e564c942e
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-07-13 10:06:37 +0600

    php date and time example

---
+++ advanced/date/date.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This is example of php date
+ */
+
+    echo "<h3>PHP Date Example</h3>";
+
+    # get today date
+    echo "Today Date is: ". date('Y-m-d')."<br>";
+    echo "Today Day is: ". date('d')."<br>";
+    echo "Today Month is: ". date('m')."<br>";
+    echo "Today Year is: ". date('Y')."<br><br>";
+
+    # get time
+    echo "Time : ". date('h:i:s A')."<br>";
