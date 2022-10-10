a0ee25abb466dc02348ba75e7eea14e2e3abb441
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-10-10 09:46:07 +0600

    PHP MySQL, SELECT and ORDER BY Clause example

---
+++ mysql_database/select/order_by_clause.php
@@ -0,0 +1,30 @@
+<!--
+    PHP MySQL Use The ORDER BY Clause
+-->
+<?php
+    $servername = "localhost";
+    $username   = "root";
+    $password   = "";
+    $dbname     = "HRMS";
+
+    # Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    # Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, name FROM Employees WHERE name='kamal' ORDER BY name";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        # output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        }
+    } else {
+        echo "Record not found";
+    }
+
+    $conn->close();
+?>
