77da86af5a479033e532033d1eec89bfa53ee0fc
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-10-30 09:24:15 +0600

    zip: get name of directory entry example

--- advanced/zip.php
+++ advanced/zip.php
@@ -10,3 +10,16 @@
     zip_read($zip);
     // close zip
     zip_close($zip);
+
+    # -------------- zip open & Get name of directory entry------------------
+    // open zip
+    $zip = zip_open("test.zip");
+
+    if ($zip) {
+        while ($zip_entry = zip_read($zip)) {
+            // Get name of directory entry
+            echo "Name: " . zip_entry_name($zip_entry) . "<br>";
+        }
+        // close zip
+        zip_close($zip);
+    }
