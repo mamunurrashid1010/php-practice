983a40bcd6a4664136743f95e7de94b0f9e94be0
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-10-31 09:35:57 +0600

    zip: get directory entry file size example

--- advanced/zip.php
+++ advanced/zip.php
@@ -23,3 +23,18 @@
         // close zip
         zip_close($zip);
     }
+
+    # -------------- zip Get the file size of directory entry------------------
+
+    $zip = zip_open("test.zip");
+
+    if ($zip) {
+        while ($zip_entry = zip_read($zip)) {
+            // directory entry name
+            echo "Name: " . zip_entry_name($zip_entry) . "<br>";
+            // Get filesize
+            echo "Filesize: " . zip_entry_filesize($zip_entry);
+        }
+        // close zip
+        zip_close($zip);
+    }
