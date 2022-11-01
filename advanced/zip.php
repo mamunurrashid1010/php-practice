3d792d4582c5f53237f72c9b79abb1629c1bd97c
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-11-01 09:33:22 +0600

    zip: open and read file of directory entry

--- advanced/zip.php
+++ advanced/zip.php
@@ -38,3 +38,23 @@
         // close zip
         zip_close($zip);
     }
+
+    # -------------- zip: open and read file of directory entry------------------
+    $zip = zip_open("test.zip");
+
+    if ($zip) {
+        while ($zip_entry = zip_read($zip)) {
+            echo "<p>Name: " . zip_entry_name($zip_entry) . "<br>";
+            // Open directory
+            if (zip_entry_open($zip, $zip_entry)) {
+                echo "File Contents:<br>";
+                // Read open directory entry
+                $contents = zip_entry_read($zip_entry);
+                echo "$contents<br>";
+                zip_entry_close($zip_entry);
+            }
+            echo "</p>";
+        }
+        // close zip
+        zip_close($zip);
+    }
